fa322f520e4b43303665242728bd09a3678737f2
Steven Ngov <gl.steven.ngov@gmail.com>
2023-03-07 15:19:18 -0500

    Build homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import About from "./components/About";
 
 import "./App.scss";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/About/index.tsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faReact,
+  faGitAlt,
+  faNode,
+} from '@fortawesome/free-brands-svg-icons';
+
+import AnimatedLetters from '../AnimatedLetters';
+
+import './index.scss';
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          consequat sapien in lacus gravida ullamcorper. Integer condimentum
+          eget erat et feugiat. Maecenas vitae laoreet ipsum. Phasellus
+          pellentesque felis et nibh fringilla euismod. Quisque auctor dapibus
+          porta. Aliquam eget rutrum magna. Proin placerat nec quam a molestie.
+          Donec sed nulla eget dolor eleifend rutrum ut a lectus. Vestibulum
+          ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Curabitur pulvinar rutrum pulvinar. Praesent semper, felis
+          lobortis sagittis maximus, diam purus ultrices metus, vitae
+          ullamcorper leo leo sit amet tortor. Donec varius, nunc ut efficitur
+          suscipit, lectus tortor malesuada orci, et suscipit augue nibh et
+          lacus. Praesent blandit massa quis ipsum laoreet sagittis.
+        </p>
+        <p>
+          Etiam in diam nec augue auctor posuere et non ligula. Maecenas
+          eleifend ex sed erat venenatis, ac viverra ligula laoreet. Nunc
+          lobortis elit nec egestas venenatis. Morbi consequat justo vitae
+          facilisis bibendum. Quisque ipsum mi, scelerisque non nunc eget,
+          venenatis imperdiet tellus. Fusce viverra turpis eget rhoncus blandit.
+          Donec volutpat sapien eu dolor vehicula, a tempus quam aliquet.
+        </p>
+        <p>
+          Integer mattis ante nisi, vel aliquet erat convallis et. Cras in
+          fermentum massa, ac mollis quam. Donec elementum diam a quam hendrerit
+          volutpat. Suspendisse pretium leo nunc, et luctus metus dapibus sit
+          amet. Vestibulum luctus velit commodo dolor lacinia tempus. Aliquam at
+          odio porttitor erat commodo blandit quis et neque. Suspendisse
+          vehicula arcu dolor, eu venenatis urna pharetra non. Aliquam ut
+          aliquam massa. Etiam fermentum, augue eget elementum sagittis, ipsum
+          mauris hendrerit ipsum, nec ullamcorper est diam sed metus. Vivamus
+          tincidunt eu justo sit amet congue.
+        </p>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faNode} color="#3C873A" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;

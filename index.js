421345af831ce1b9b63b6d40f14261ee4c76a59d
Steven Ngov <gl.steven.ngov@gmail.com>
2023-03-25 11:40:28 -0400

    Updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { children: _jsx(App, {}) }) }));
+root.render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { basename: process.env.PUBLIC_URL, children: _jsx(App, {}) }) }));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

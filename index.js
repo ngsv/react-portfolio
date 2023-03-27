ecae66767deba461eb174f5c81a6448729a41509
Steven Ngov <gl.steven.ngov@gmail.com>
2023-03-27 16:20:29 -0400

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import { jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import App from './App';
 import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { basename: process.env.PUBLIC_URL, children: _jsx(App, {}) }) }));
+root.render(_jsx(React.StrictMode, { children: _jsxs(BrowserRouter, { basename: "/react-portfolio", children: [_jsx(ScrollToTop, {}), _jsx(App, {})] }) }));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
